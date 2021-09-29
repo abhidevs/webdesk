82046c7ebe1ea6ca6348700b2929ecc084ceec14
Bisu03 <berabiswanath338@gmail.com>
2021-09-29 20:51:46 +0530

    dashbord in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import Herosection from './components/herosection/Herosection';
+import Notemeterial from './components/notemeterial/Notemeterial';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Welcome to WebDesk</h1>
+      {/* <h1>Welcome to WebDesk</h1> */}
+      <Herosection/>
+      <Notemeterial/>
     </div>
+    
+
+    
   );
 }
 
