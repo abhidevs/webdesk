768e3f074326b6589b76ba4454e4a687baf85650
Bisu03 <berabiswanath338@gmail.com>
2021-11-01 14:33:47 +0530

    added background image and modify style.scss of login and register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import "./style.scss";
+import registerimg from "../../assets/reg-side-img.svg";
 // import axios from "axios";
 import { useHistory } from "react-router-dom";
+import  Person  from "@material-ui/icons/Person";
+import Email from "@material-ui/icons/Email";
+import Lock from "@material-ui/icons/Lock";
 
 const Register = () => {
   const history = useHistory();
@@ -35,36 +39,51 @@ const Register = () => {
 
   return (
     <div className="register">
-      <div className="container">
+      <div className="side-img">
+        <img src={registerimg} alt="" />
+      </div>
+      <div className="register-page">
         <h1>Register</h1>
-        <input
-          type="text"
-          name="name"
-          value={user.name}
-          placeholder="Your Name"
-          onChange={handleChange}
-        ></input>
-        <input
-          type="text"
-          name="email"
-          value={user.email}
-          placeholder="Your Email"
-          onChange={handleChange}
-        ></input>
-        <input
-          type="password"
-          name="password"
-          value={user.password}
-          placeholder="Your Password"
-          onChange={handleChange}
-        ></input>
-        <input
-          type="password"
-          name="reEnterPassword"
-          value={user.reEnterPassword}
-          placeholder="Re-enter Password"
-          onChange={handleChange}
-        ></input>
+        <div className="register-input">
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            placeholder="Your Name"
+            onChange={handleChange}
+          ></input>
+          <Person className="icon"/>
+        </div>
+        <div className="register-input">
+          <input
+            type="text"
+            name="email"
+            value={user.email}
+            placeholder="Your Email"
+            onChange={handleChange}
+          ></input>
+          <Email className="icon"/>
+        </div>
+        <div className="register-input">
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            placeholder="Your Password"
+            onChange={handleChange}
+          ></input>
+          <Lock className="icon"/>
+        </div>
+        <div className="register-input">
+          <input
+            type="password"
+            name="reEnterPassword"
+            value={user.reEnterPassword}
+            placeholder="Re-enter Password"
+            onChange={handleChange}
+          ></input>
+          <Lock className="icon"/>
+        </div>
         <div className="btn" onClick={register}>
           Register
         </div>
