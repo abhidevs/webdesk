fb2e0bb5d91eec02eed698ebd940e6bec8206bb7
Abhik Das <abhikdas98765@gmail.com>
2021-11-27 00:07:38 +0530

    added responsive ui & styles for chats, participants and meeting info

--- client/src/pages/onlinemeet/Onlinemeet.jsx
+++ client/src/pages/onlinemeet/Onlinemeet.jsx
@@ -9,27 +9,43 @@ import CancelPresentationRoundedIcon from "@material-ui/icons/CancelPresentation
 import CommentIcon from "@material-ui/icons/Comment";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import SendIcon from "@material-ui/icons/Send";
+import MoreHorizRoundedIcon from "@material-ui/icons/MoreHorizRounded";
+import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 import VideoChat from "../../components/videoChat/VideoChat";
 import CallEndIcon from "@material-ui/icons/CallEnd";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { useParams } from "react-router-dom";
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 import Peer from "simple-peer";
 import { AuthContext } from "../../context/authContext/AuthContext";
-import { FormControlLabel } from "@material-ui/core";
+import dummyProfilePic from "../../assets/dummyProfilePic.png";
+import {
+  Button,
+  IconButton,
+  Slide,
+  Dialog,
+  DialogActions,
+  makeStyles,
+} from "@material-ui/core";
 
 const Video = ({ peer, userVideo, videoRef }) => {
   const ref = useRef();
 
   useEffect(() => {
-    if(!userVideo) {
+    if (!userVideo) {
       peer.on("stream", (stream) => {
         ref.current.srcObject = stream;
       });
     }
   }, []);
 
-  return userVideo ? <video playsInline autoPlay ref={videoRef} /> : <video playsInline autoPlay ref={ref} />;
+  return userVideo ? (
+    <video playsInline autoPlay ref={videoRef} />
+  ) : (
+    <video playsInline autoPlay ref={ref} />
+  );
 };
 
 const videoConstraints = {
@@ -37,21 +53,54 @@ const videoConstraints = {
   width: window.innerWidth / 2,
 };
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+let DialogBtnStyle = {
+  width: "50px",
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  backgroundColor: "var(--secondary-color)",
+  border: "none",
+  borderRadius: "50px",
+  boxShadow: "2px 2px 20px #d1d1d1",
+  color: "var(--text-color2)",
+  margin: "10px",
+};
+
+const useStyles = makeStyles({
+  topScrollPaper: {
+    alignItems: "flex-end",
+  },
+  topPaperScrollBody: {
+    verticalAlign: "bottom",
+  },
+});
+
 const Onlinemeet = () => {
   const [peers, setPeers] = useState([]);
   const [message, setMessage] = useState("");
   const [allMessages, setAllMessages] = useState([]);
   const [audioEnabled, setAudioEnabled] = useState(true);
   const [videoEnabled, setVideoEnabled] = useState(true);
-  const [screenShareEnabled, setScreenShareEnabled] = useState(false)
+  const [screenShareEnabled, setScreenShareEnabled] = useState(false);
+  const [activeRightPanelTab, setActiveRightPanelTab] = useState("chats");
+  const [moreControlsOpened, setMoreControlsOpened] = useState(false);
+  const [fullScreenRightPanelOpened, setFullScreenRightPanelOpened] =
+    useState(false);
   const socketRef = useRef();
   const userVideo = useRef();
   const screenCapture = useRef();
   const peersRef = useRef([]);
-  const {classId} = useParams();
+  const { classId } = useParams();
 
   const { user } = useContext(AuthContext);
-  
+  const classes = useStyles();
+
   useEffect(() => {
     const roomID = classId;
     console.log(roomID);
@@ -61,16 +110,24 @@ const Onlinemeet = () => {
       .then((stream) => {
         userVideo.current.srcObject = stream;
         console.log("initiated socket", roomID);
-        socketRef.current.emit("join room", {roomID, username: user.fullname, userProfilePic: user.profilePic });
+        socketRef.current.emit("join room", {
+          roomID,
+          username: user.fullname,
+          userProfilePic: user.profilePic,
+        });
         socketRef.current.on("all users", (users) => {
           const peers = [];
           users.forEach((user) => {
-            const peer = createPeer(user.socketID, socketRef.current.id, stream);
+            const peer = createPeer(
+              user.socketID,
+              socketRef.current.id,
+              stream
+            );
             peersRef.current.push({
               peerID: user.socketID,
               peer,
             });
-            peers.push({...user, peer});
+            peers.push({ ...user, peer });
           });
 
           setPeers(peers);
@@ -84,7 +141,7 @@ const Onlinemeet = () => {
             peer,
           });
 
-          setPeers((users) => [...users, {peer, ...payload.userInfo}]);
+          setPeers((users) => [...users, { peer, ...payload.userInfo }]);
         });
 
         socketRef.current.on("receiving returned signal", (payload) => {
@@ -96,9 +153,29 @@ const Onlinemeet = () => {
           setAllMessages((msgs) => [...msgs, payload.message]);
         });
       });
+
+    document.getElementById(activeRightPanelTab).classList.add("active");
   }, []);
 
   console.log(peers, peersRef);
+
+  const changeactiveRightPanelTab = (e, tab) => {
+    document.getElementById(activeRightPanelTab).classList.remove("active");
+    let currentActive;
+    if (e) currentActive = e.currentTarget.id;
+    else currentActive = tab;
+    setActiveRightPanelTab(currentActive);
+    document.getElementById(currentActive).classList.add("active");
+  };
+
+  const openFullScreenRightPanel = (tab) => {
+    changeactiveRightPanelTab(null, tab);
+    document.getElementById("rightPanel").classList.add("fullscreen");
+  };
+
+  const closeFullScreenRightPanel = () => {
+    document.getElementById("rightPanel").classList.remove("fullscreen");
+  };
 
   function createPeer(userToSignal, callerID, stream) {
     const peer = new Peer({
@@ -136,22 +213,22 @@ const Onlinemeet = () => {
 
   const sendMessage = (e) => {
     e.preventDefault();
-    
+
     const msgInfo = {
       message,
       type: "sent",
       senderName: user.fullname,
       senderProfilePic: user.profilePic,
       timeOfSending: new Date(),
-    }
-    
+    };
+
     setMessage("");
-    setAllMessages((msgs) => [...msgs, {...msgInfo}]);
+    setAllMessages((msgs) => [...msgs, { ...msgInfo }]);
     // msgInfo.type = "received";
 
     peersRef.current.forEach((peer) => {
       socketRef.current.emit("sending message", {
-        message: {...msgInfo, type: "received"},
+        message: { ...msgInfo, type: "received" },
         userToSignal: peer.peerID,
         senderID: "Abhik",
       });
@@ -160,59 +237,80 @@ const Onlinemeet = () => {
 
   const muteUnmuteAudio = () => {
     const enabled = userVideo.current.srcObject.getAudioTracks()[0].enabled;
-    if(enabled) {
+    if (enabled) {
       userVideo.current.srcObject.getAudioTracks()[0].enabled = false;
       setAudioEnabled(false);
     } else {
       userVideo.current.srcObject.getAudioTracks()[0].enabled = true;
       setAudioEnabled(true);
     }
-  }
+  };
 
   const muteUnmuteVideo = () => {
     const enabled = userVideo.current.srcObject.getVideoTracks()[0].enabled;
-    if(enabled) {
+    if (enabled) {
       userVideo.current.srcObject.getVideoTracks()[0].enabled = false;
       setVideoEnabled(false);
     } else {
       userVideo.current.srcObject.getVideoTracks()[0].enabled = true;
       setVideoEnabled(true);
     }
-  }
+  };
 
   const toggleScreenSharing = async () => {
     if (!screenShareEnabled) {
       try {
-        screenCapture.current = await navigator.mediaDevices.getDisplayMedia({ video: true });
+        screenCapture.current = await navigator.mediaDevices.getDisplayMedia({
+          video: true,
+        });
         screenCapture.current.getTracks()[0].onended = switchBackToUserVideo;
         userVideo.current.srcObject = screenCapture.current;
         setScreenShareEnabled(true);
 
-        peersRef.current.forEach(({peer}) => {
+        peersRef.current.forEach(({ peer }) => {
           peer.streams[0].getVideoTracks()[0].stop();
-          console.log(peer.streams[0].getVideoTracks()[0], screenCapture.current.getVideoTracks()[0]);
-          peer.replaceTrack(peer.streams[0].getVideoTracks()[0], screenCapture.current.getVideoTracks()[0], peer.streams[0]);
-        })
+          console.log(
+            peer.streams[0].getVideoTracks()[0],
+            screenCapture.current.getVideoTracks()[0]
+          );
+          peer.replaceTrack(
+            peer.streams[0].getVideoTracks()[0],
+            screenCapture.current.getVideoTracks()[0],
+            peer.streams[0]
+          );
+        });
       } catch (err) {
-        console.error('error occured when trying to get screen sharing stream', err);
+        console.error(
+          "error occured when trying to get screen sharing stream",
+          err
+        );
       }
     } else {
-      screenCapture.current.getTracks().forEach(track => track.stop());
+      screenCapture.current.getTracks().forEach((track) => track.stop());
       switchBackToUserVideo();
     }
   };
-  
+
   const switchBackToUserVideo = async () => {
     setScreenShareEnabled(false);
-    userVideo.current.srcObject = await navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true });
-    if(!audioEnabled) userVideo.current.srcObject.getAudioTracks()[0].enabled = false;
-    if(!videoEnabled) userVideo.current.srcObject.getVideoTracks()[0].enabled = false;
-    
-    peersRef.current.forEach(({peer}) => {
+    userVideo.current.srcObject = await navigator.mediaDevices.getUserMedia({
+      video: videoConstraints,
+      audio: true,
+    });
+    if (!audioEnabled)
+      userVideo.current.srcObject.getAudioTracks()[0].enabled = false;
+    if (!videoEnabled)
+      userVideo.current.srcObject.getVideoTracks()[0].enabled = false;
+
+    peersRef.current.forEach(({ peer }) => {
       peer.streams[0].getVideoTracks()[0].stop();
-      peer.replaceTrack(peer.streams[0].getVideoTracks()[0], userVideo.current.srcObject.getVideoTracks()[0], peer.streams[0]);
-    })
-  }
+      peer.replaceTrack(
+        peer.streams[0].getVideoTracks()[0],
+        userVideo.current.srcObject.getVideoTracks()[0],
+        peer.streams[0]
+      );
+    });
+  };
 
   return (
     <div className="onlinemeet">
@@ -225,10 +323,18 @@ const Onlinemeet = () => {
               {audioEnabled ? <MicRoundedIcon /> : <MicOffRoundedIcon />}
             </button>
             <button className="btn" onClick={muteUnmuteVideo}>
-              { videoEnabled ? <VideocamRoundedIcon /> : <VideocamOffRoundedIcon />}
+              {videoEnabled ? (
+                <VideocamRoundedIcon />
+              ) : (
+                <VideocamOffRoundedIcon />
+              )}
             </button>
             <button className="btn mobile-hide" onClick={toggleScreenSharing}>
-              { screenShareEnabled ? <CancelPresentationRoundedIcon /> : <PresentToAllRoundedIcon />}
+              {screenShareEnabled ? (
+                <CancelPresentationRoundedIcon />
+              ) : (
+                <PresentToAllRoundedIcon />
+              )}
             </button>
             <button className="btn mobile-hide">
               <CommentIcon />
@@ -236,24 +342,82 @@ const Onlinemeet = () => {
             <button className="btn mobile-hide">
               <SupervisorAccountIcon />
             </button>
-            <button className="btn moreBtn">
+            <button
+              className="btn moreBtn"
+              onClick={() => setMoreControlsOpened(true)}
+            >
               <MoreVertIcon />
             </button>
             <button className="leaveBtn">
               <CallEndIcon />
             </button>
+
+            <Dialog
+              open={moreControlsOpened}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={() => setMoreControlsOpened(false)}
+              aria-describedby="more options"
+              classes={{
+                scrollPaper: classes.topScrollPaper,
+                paperScrollBody: classes.topPaperScrollBody,
+              }}
+            >
+              <DialogActions>
+                <button
+                  style={DialogBtnStyle}
+                  onClick={() => {
+                    setMoreControlsOpened(false);
+                    toggleScreenSharing();
+                  }}
+                >
+                  {screenShareEnabled ? (
+                    <CancelPresentationRoundedIcon />
+                  ) : (
+                    <PresentToAllRoundedIcon />
+                  )}
+                </button>
+                <button
+                  style={DialogBtnStyle}
+                  onClick={() => {
+                    setMoreControlsOpened(false);
+                    openFullScreenRightPanel("chats");
+                  }}
+                >
+                  <CommentIcon />
+                </button>
+                <button
+                  style={DialogBtnStyle}
+                  onClick={() => {
+                    setMoreControlsOpened(false);
+                    openFullScreenRightPanel("participants");
+                  }}
+                >
+                  <SupervisorAccountIcon />
+                </button>
+                <button
+                  style={DialogBtnStyle}
+                  onClick={(e) => {
+                    setMoreControlsOpened(false);
+                    openFullScreenRightPanel("info");
+                  }}
+                >
+                  <InfoOutlinedIcon />
+                </button>
+              </DialogActions>
+            </Dialog>
           </div>
         </div>
 
-        <div className="participants-section">
+        <div className="screens-section">
           <div className="heading">
             <SupervisorAccountIcon />
-            (24) All Participants
+            ({peers.length}) All Screens
           </div>
 
-          <div className="participants">
-            {peers.map(({username, peer}, index) => (
-              <div className="participant">
+          <div className="screens">
+            {peers.map(({ username, peer }, index) => (
+              <div className="screen">
                 <Video key={index} peer={peer} />
                 <p>{username.split(" ")[0]}</p>
               </div>
@@ -261,33 +425,109 @@ const Onlinemeet = () => {
           </div>
         </div>
 
-        <div className="chat-section">
+        <div className="right-panel" id="rightPanel">
           <div className="topSection">
-            <p className="chatNavLink">chats</p>
-            <p className="chatNavLink">participants</p>
-            <p className="chatNavLink">info</p>
+            <button className="backBtn" onClick={closeFullScreenRightPanel}>
+              <ArrowBackRoundedIcon className="icon" />
+            </button>
+
+            <p
+              className="rightNavLink"
+              id="chats"
+              onClick={changeactiveRightPanelTab}
+            >
+              chats
+            </p>
+            <p
+              className="rightNavLink"
+              id="participants"
+              onClick={changeactiveRightPanelTab}
+            >
+              participants
+            </p>
+            <p
+              className="rightNavLink"
+              id="info"
+              onClick={changeactiveRightPanelTab}
+            >
+              info
+            </p>
           </div>
 
           <div className="middleSection">
-            <div className="chats">
-              {allMessages?.map((m) => (
-                <VideoChat
-                  type={m.type}
-                  senderName={m.senderName}
-                  senderProfilePic={m.senderProfilePic}
-                  message={m.message}
-                  timeOfSending={m.timeOfSending}
-                />
-              ))}
-            </div>
-          </div>
+            {activeRightPanelTab === "participants" && (
+              <div className="participants">
+                <div className="participant">
+                  <img
+                    src={user.profilePic || dummyProfilePic}
+                    alt="user profile"
+                  />
+                  <h6>{user.fullname} (You)</h6>
+                  <IconButton>
+                    <MoreHorizRoundedIcon />
+                  </IconButton>
+                </div>
 
-          <form className="bottomSection" onSubmit={sendMessage}>
-            <input type="text" placeholder="type message" value={message} onChange={(e) => setMessage(e.target.value)} />
-            <button>
-              <SendIcon />
-            </button>
-          </form>
+                {peers?.map((p) => (
+                  <div className="participant">
+                    <img
+                      src={p.profilePic || dummyProfilePic}
+                      alt="user profile"
+                    />
+                    <h6>{p.username}</h6>
+                    <IconButton>
+                      <MoreHorizRoundedIcon />
+                    </IconButton>
+                  </div>
+                ))}
+              </div>
+            )}
+
+            {activeRightPanelTab === "chats" && (
+              <>
+                <div className="chats">
+                  {allMessages?.map((m) => (
+                    <VideoChat
+                      type={m.type}
+                      senderName={m.senderName}
+                      senderProfilePic={m.senderProfilePic}
+                      message={m.message}
+                      timeOfSending={m.timeOfSending}
+                    />
+                  ))}
+                </div>
+                <form className="msgForm" onSubmit={sendMessage}>
+                  <input
+                    type="text"
+                    placeholder="type message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                  <button disabled={!message.length}>
+                    <SendIcon />
+                  </button>
+                </form>
+              </>
+            )}
+
+            {activeRightPanelTab === "info" && (
+              <div className="meeting-info">
+                <h6>Joining info</h6>
+                <p>{window.location.href}</p>
+                <Button
+                  variant="text"
+                  color="primary"
+                  startIcon={<ContentCopyRoundedIcon />}
+                  style={{ textTransform: "none" }}
+                  onClick={() =>
+                    navigator.clipboard.writeText(window.location.href)
+                  }
+                >
+                  Copy joining info
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
